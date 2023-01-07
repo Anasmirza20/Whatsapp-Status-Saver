2c7e4b2996a6300f2a67c16884291530805f68c1
Anas Mirza <anas.mirza@rooter.io>
2023-01-07 11:51:04 +0530

    Icon and design chnages

--- app/src/main/java/com/whatsappstatus/HomeActivity.kt
+++ app/src/main/java/com/whatsappstatus/HomeActivity.kt
@@ -2,6 +2,7 @@ package com.whatsappstatus
 
 import android.Manifest.permission.READ_EXTERNAL_STORAGE
 import android.Manifest.permission.WRITE_EXTERNAL_STORAGE
+import android.content.Intent
 import android.os.Build
 import android.os.Bundle
 import android.util.Log
@@ -16,6 +17,13 @@ import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.gms.ads.AdRequest
 import com.google.android.gms.ads.MobileAds
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.play.core.appupdate.AppUpdateManager
+import com.google.android.play.core.appupdate.AppUpdateManagerFactory
+import com.google.android.play.core.install.InstallStateUpdatedListener
+import com.google.android.play.core.install.model.AppUpdateType
+import com.google.android.play.core.install.model.InstallStatus
+import com.google.android.play.core.install.model.UpdateAvailability
 import com.whatsapp.reelsfiz.R
 import com.whatsapp.reelsfiz.databinding.ActivityMainBinding
 import com.whatsappstatus.util.Utils
@@ -25,6 +33,8 @@ import javax.inject.Inject
 
 @AndroidEntryPoint
 class HomeActivity : AppCompatActivity() {
+    private var appUpdateManager: AppUpdateManager? = null
+    private val UPDATE_REQUEST_CODE: Int = 121
     private lateinit var binding: ActivityMainBinding
 
     @Inject
@@ -39,6 +49,7 @@ class HomeActivity : AppCompatActivity() {
         MobileAds.initialize(this) {
 
         }
+        checkAppUpdates()
         binding.apply {
             if (isWhatsappInstalled) {
                 manageNavigation()
@@ -63,6 +74,7 @@ class HomeActivity : AppCompatActivity() {
     override fun onDestroy() {
         super.onDestroy()
         getExoPlayer()?.release()
+        appUpdateManager?.unregisterListener(listener)
     }
 
     private fun setObserver() {
@@ -111,4 +123,94 @@ class HomeActivity : AppCompatActivity() {
     }
 
 
+    private fun checkAppUpdates() {
+        appUpdateManager = AppUpdateManagerFactory.create(this)
+
+// Returns an intent object that you use to check for an update.
+        val appUpdateInfoTask = appUpdateManager?.appUpdateInfo
+
+// Checks that the platform will allow the specified type of update.
+        appUpdateInfoTask?.addOnSuccessListener { appUpdateInfo ->
+            if (appUpdateInfo.updateAvailability() == UpdateAvailability.UPDATE_AVAILABLE
+                // This example applies an immediate update. To apply a flexible update
+                // instead, pass in AppUpdateType.FLEXIBLE
+                && appUpdateInfo.isUpdateTypeAllowed(AppUpdateType.FLEXIBLE)
+            ) {
+                appUpdateManager?.registerListener(listener)
+
+                appUpdateManager?.startUpdateFlowForResult(
+                    // Pass the intent that is returned by 'getAppUpdateInfo()'.
+                    appUpdateInfo,
+                    // Or 'AppUpdateType.FLEXIBLE' for flexible updates.
+                    AppUpdateType.FLEXIBLE,
+                    // The current activity making the update request.
+                    this,
+                    // Include a request code to later monitor this update request.
+                    UPDATE_REQUEST_CODE
+                )
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == UPDATE_REQUEST_CODE) {
+            if (resultCode != RESULT_OK) {
+                Log.e("MY_APP", "Update flow failed! Result code: $resultCode")
+                // If the update is cancelled or fails,
+                // you can request to start the update again.
+                checkAppUpdates()
+            }
+        }
+    }
+
+    private val listener = InstallStateUpdatedListener { state ->
+        // (Optional) Provide a download progress bar.
+        when (state.installStatus()) {
+            InstallStatus.DOWNLOADING -> {
+                val bytesDownloaded = state.bytesDownloaded()
+                val totalBytesToDownload = state.totalBytesToDownload()
+            }
+            InstallStatus.CANCELED -> {
+
+            }
+            InstallStatus.DOWNLOADED -> {
+                // After the update is downloaded, show a notification
+                // and request user confirmation to restart the app.
+                popupSnackBarForCompleteUpdate()
+            }
+            InstallStatus.FAILED -> {
+
+            }
+            InstallStatus.INSTALLED -> {
+
+            }
+            InstallStatus.INSTALLING -> {
+
+            }
+            InstallStatus.PENDING -> {
+
+            }
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        appUpdateManager?.appUpdateInfo?.addOnSuccessListener { appUpdateInfo ->
+            if (appUpdateInfo.installStatus() == InstallStatus.DOWNLOADED) {
+                popupSnackBarForCompleteUpdate()
+            }
+        }
+    }
+
+    private fun popupSnackBarForCompleteUpdate() {
+        Snackbar.make(
+            findViewById(R.id.main_layout),
+            "An update has just been downloaded.",
+            Snackbar.LENGTH_INDEFINITE
+        ).apply {
+            setAction("RESTART") { appUpdateManager?.completeUpdate() }
+            show()
+        }
+    }
 }
